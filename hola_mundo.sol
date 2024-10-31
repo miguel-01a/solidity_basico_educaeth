e97f47d4ddbea9ea12f9c6fcb67ddb00c51de260
miguel-01a <eduardomgval01@gmail.com>
2024-10-31 00:09:13 -0600

    primer contrato

---
+++ hola_mundo.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// 0x070933D1abC81C2D85031CFa3b251BA4cFdB13d8
+
+pragma solidity 0.8.26; // en esta parte se define la version del compilador
+
+//a partir de aquí  coloco la lógica del contrado
+contract hola_mundo {
+
+    string public saludo  = "Hola desde educaeth"; //declarando el contenido de mi variable saludo
+
+}
